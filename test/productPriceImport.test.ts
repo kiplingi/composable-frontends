bd69fe30f59e47616d5d9dcee4cbab6b70f71418
Alexander Schmidt <support@kiplingi.de>
2023-12-04 16:10:20 +0200

    actions 1

--- test/productPriceImport.test.ts
+++ test/productPriceImport.test.ts
@@ -1,4 +1,4 @@
 test('Product price component import', async () => {
-    const cmp = await import('../components/product/ProductPrice1.vue')
+    const cmp = await import('../components/product/ProductPrice.vue')
     expect(cmp).toBeDefined()
 })
